ed6b1caff3f24ef361f3b319a867c4edd27986cd
Italo Freire <italo.freire.gonzalez@gmail.com>
2025-08-26 20:51:00 -0400

    Update Cliente.java
    Se agrega getters y setters

--- Cliente.java
+++ Cliente.java
@@ -12,5 +12,27 @@ public class Cliente {
         this.rut = rut;
         this.correo = correo;
     }
+
+        public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getRut(){
+        return this.rut;
+    }
+    public void setRut(String rut){
+        this.rut = rut;
+    }
+    
+    public String getCorreo(){
+        return this.correo;
+    }
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
 }
+
 
