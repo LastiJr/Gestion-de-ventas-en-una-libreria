249c5b1acd43cd23a3b9ab91c4472f0b9ebb3c0a
Matías Arancibia Becerra <164715599+Mawfu89@users.noreply.github.com>
2025-09-26 03:03:56 -0300

    Clase Libro se cambia de package, se agrega editorial
    Se agrega editorial, sus get y set.
    Se cambia el package para el uso de javafx

--- Libro.java
+++ Libro.java
@@ -1,4 +1,4 @@
-package libreria;
+package com.mycompany.gestionlibreria;
 
 public class Libro {
     
@@ -7,51 +7,74 @@ public class Libro {
     private double precio;
     private int stock;
     private String isbn;
+    private String editorial;
     
-    public Libro(String titulo, String autor, double precio, int stock, String isbn){
-        
+
+    public Libro(String titulo, String autor, double precio, int stock, String isbn, String editorial)
+    {
         this.titulo = titulo;
         this.autor = autor;
         this.precio = precio;
         this.stock = stock;
         this.isbn = isbn;
+        this.editorial = editorial;
     }
     
-    public String getTitulo(){
+    public String getTitulo()
+    {
         return this.titulo;
     }
-    public void setTitulo(String titulo){
+    public void setTitulo(String titulo)
+    {
         this.titulo = titulo;
     }
     
-    public String getAutor(){
+    public String getAutor()
+    {
         return this.autor;
     }
-    public void setAutor(String autor){
+    public void setAutor(String autor)
+    {
         this.autor = autor;
     }
     
-    public double getPrecio(){
+    public double getPrecio()
+    {
         return this.precio;
     }
-    public void setPrecio(double precio){
+    public void setPrecio(double precio)
+    {
         this.precio = precio;
     }
     
-    public int getStock(){
+    public int getStock()
+    {
         return this.stock;
     }
-    public void setStock(int stock){
+    public void setStock(int stock)
+    {
         this.stock = stock;
     }
     
-    public String getIsbn(){
+    public String getIsbn()
+    {
         return this.isbn;
     }
-    public void setIsbn(String isbn){
+    public void setIsbn(String isbn)
+    {
         this.isbn = isbn;
     }
 
+    public String getEditorial() 
+    {
+        return editorial;
+    }
+    public void setEditorial(String editorial) 
+    {
+        this.editorial = editorial;
+    }
+
+    
     public void ajustarStock(int delta)
     {
         int nuevo = this.stock + delta;
@@ -60,11 +83,9 @@ public class Libro {
     }
 
     @Override
-    public String toString()
+    public String toString() 
     {
-        return String.format("[%s] \"%s\" - %s | $%.0f | stock: %d",isbn, titulo, autor, precio, stock);
+        return String.format("[%s] \"%s\" - %s (%s) | $%.0f | stock: %d",
+                isbn, titulo, autor, editorial, precio, stock);
     }
-
-    
-    
 }
